d686463671822f42be2eb540c00bca8cd91f243b
gauravkhanna3007 <gaurav3007@gmail.com>
2023-12-06 23:21:31 +0530

    trying myself

--- script/add_permissions_block.py
+++ script/add_permissions_block.py
@@ -1,24 +1,20 @@
 import sys
-from ruamel.yaml import YAML, comments
-from ruamel.yaml.scalarstring import DoubleQuotedScalarString as dq
+from ruamel.yaml import YAML
 
 def add_permissions_block(file_path):
-    yaml = YAML()
-    yaml.indent(offset=2)  # Set the indentation offset
-
     with open(file_path, 'r') as file:
+        yaml = YAML()
         data = yaml.load(file)
 
-    permissions_block = comments.CommentedMap()
-    permissions_block['permissions'] = dq('write-all')
+    permissions_block = {
+        'permissions': 'write-all'
+    }
 
     # Check if 'on' block exists
     if 'on' in data:
         # Insert 'permissions' block after 'on' block
-        index_on = list(data).index('on') + 1
-        data.insert(index_on, ('', permissions_block))
-        data.insert(index_on + 1, comments.CommentedMap())
-        data.insert(index_on + 2, comments.CommentedMap())
+        index = list(data).index('on') + 1
+        data.insert(index, 'permissions', 'write-all')
 
         with open(file_path, 'w') as file:
             yaml.dump(data, file)
