24d9abf6b9dfb4302a490fd24e9116e5cbbe86d1
gauravkhanna3007 <gaurav3007@gmail.com>
2023-12-07 20:43:55 +0530

    further testing

--- script/add_permissions_block.py
+++ script/add_permissions_block.py
@@ -10,21 +10,30 @@ def add_permissions_block(file_path):
         'permissions': 'write-all'
     }
 
-    # Check if 'on' block and 'permissions' block exist
-    if 'on' in data and 'permissions' not in data:
-        # Insert 'permissions' block after 'on' block
-        index = list(data).index('on') + 1
-        data.yaml_set_comment_before_after_key('permissions', before='\n')
-        data.insert(index, 'permissions', 'write-all')
-        
+    # Check if 'permissions' block exists at any level
+    if not check_permissions_exist(data):
+        # Insert 'permissions' block at the top level
+        data.insert(0, 'permissions', 'write-all')
+
         with open(file_path, 'w') as file:
             yaml.dump(data, file)
         
         print(f"Added permissions block to {file_path}")
-    elif 'permissions' in data:
-        print(f"'permissions:' block already exists in {file_path}. Skipping...")
     else:
-        print(f"'on:' block not found in {file_path}. Skipping...")
+        print(f"'permissions:' block already exists in {file_path}. Skipping...")
+
+def check_permissions_exist(data):
+    if isinstance(data, dict):
+        if 'permissions' in data:
+            return True
+        for key, value in data.items():
+            if check_permissions_exist(value):
+                return True
+    elif isinstance(data, list):
+        for item in data:
+            if check_permissions_exist(item):
+                return True
+    return False
 
 if __name__ == "__main__":
     file_path = sys.argv[1]
