40018c6daf311f4a0c7d70a5c6a0ada190dd82a2
gauravkhanna3007 <gaurav3007@gmail.com>
2023-12-11 22:39:50 +0530

    no luck as of now

--- script/add_permissions_block.py
+++ script/add_permissions_block.py
@@ -7,13 +7,24 @@ def add_permissions_block(file_path):
     with open(file_path, 'r') as f:
         content = YAML().load(f)
 
-    # Check if 'on' block exists and if 'permissions' block is not present
-    if 'on' in content and 'permissions' not in content:
+    # Check if 'on' block exists and if 'permissions' block is not present in jobs
+    if 'on' in content and ('jobs' not in content or 'permissions' not in content['jobs']):
         # Add a blank line for readability
-        content.insert(content.index('on') + 1, 'permissions', 'write-all')
+        if 'jobs' in content and 'permissions' in content['jobs']:
+            jobs = content['jobs']
+            if isinstance(jobs, list):
+                for job in jobs:
+                    if 'permissions' not in job:
+                        job.yaml_set_comment_before_after_key('permissions', before='\n')
+                        job.insert(list(job).index('name') + 1, 'permissions', 'write-all')
+            else:
+                jobs.yaml_set_comment_before_after_key('permissions', before='\n')
+                jobs.insert(list(jobs).index('name') + 1, 'permissions', 'write-all')
+        else:
+            content['permissions'] = 'write-all'
 
         with open(file_path, 'w') as f:
-            YAML().dump(content, f)
+            YAML().dump(content, f, default_flow_style=False)
 
 def process_workflow_files():
     # Get a list of all .yml files in the .github/workflows directory
