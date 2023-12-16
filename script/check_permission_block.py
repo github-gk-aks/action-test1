775cd547152c712837ac51bd240145387cb0a185
gauravkhanna3007 <gaurav3007@gmail.com>
2023-12-16 19:25:01 +0530

    Changing the approach

--- script/check_permission_block.py
+++ script/check_permission_block.py
@@ -15,6 +15,9 @@ def check_permissions_block(file_path):
         for job_name, job_content in content['jobs'].items():
             if 'permissions' in job_content:
                 print(f"Permissions block found in job '{job_name}' in {file_path}")
+    
+    if 'permissions' not in content and 'jobs' not in content:
+        print(f"Permissions block not found in {file_path}")
 
 def process_workflow_files():
     # Get a list of all .yml files in the .github/workflows directory
